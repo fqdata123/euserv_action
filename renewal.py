c8fc6f22afd710832ea21a871e50393ca644e95d
fqdata123 <93412694+fqdata123@users.noreply.github.com>
2022-07-31 09:40:31 +0800

    Update renewal.py

--- renewal.py
+++ renewal.py
@@ -204,7 +204,6 @@ def get_pin_from_mailparser(url_id: str) -> str:
     #body = response.json()[0]["body"].replace("\n","").replace("\r","")
     response.encoding="utf-8"
     body = response.text.replace("\n","").replace("\r","")
-    body = response.text
     pin = re.findall("PIN\D?\D?(\d+)",body)[0]
     return pin
 
